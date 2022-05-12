6ed841aebc5ac88c0bb92ab862b7329c3eebff10
guidojuliano <guidojulianou@gmail.com>
2022-05-12 20:33:08 -0300

    push

---
+++ src/components/Noticias.js
@@ -0,0 +1,12 @@
+function Noticias() {
+  var titulo = "Titulo Noticia";
+  var descripcion = "Lorem";
+  return (
+    <section id="section-1">
+      <h1>{titulo}</h1>
+      <h3>{descripcion}</h3>
+    </section>
+  );
+}
+
+export default Noticias;
